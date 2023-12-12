8dec300093a23faa11e04f43c4fd628fa10898d9
bruno kreppel <bruno.kreppel@gmail.com>
2023-12-12 13:04:30 +0100

    added index structure

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/rootstyles.css">
+    <link rel="stylesheet" href="style/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
+
+</head>
+<body>
+
+    <div class="about_wrapper">
+        <div class="about_container">
+            Test
+        </div>
+        <div class="about_container">
+            Test
+        </div>
+    </div>
+
+<div class="services_wrapper">
+    <div class="row">
+        <div class="col-md-4 col-sm-6">
+            <div class="services_card"></div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="services_card"></div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="services_card"></div>
+        </div>
+    </div>
+</div>
+
+<div class="uni_wrapper">
+<i class="ri-test-tube-fill"></i>
+<i class="ri-test-tube-fill"></i>
+<i class="ri-test-tube-fill"></i>
+<i class="ri-test-tube-fill"></i>
+<i class="ri-test-tube-fill"></i>
+</div>
+
+
+<div class="tutor_wrapper">
+
+</div>
+
+<div class="review_wrapper">
+
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    
+</body>
+</html>
