e2c201301f3a9db0f08233130c66f3e3e4827a62
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-12 10:30:03 +0100

    adding file_upload.php

---
+++ components/file_upload.php
@@ -0,0 +1,33 @@
+<?php
+   function fileUpload($image, $source = 'user'){
+
+       $message = "";
+
+       if($image["error"] == 4){ 
+           $imageName = "User-avatar.svg.png";
+
+           if($source == "courses"){
+               $imageName = "Course.png";
+           }
+
+           $message = "No picture has been chosen, please upload your image later :)";
+       }else{
+           $checkIfImage = getimagesize($image["tmp_name"]); 
+           $message = $checkIfImage ? "Ok" : "Not an image";
+       }
+
+       if($message == "Ok"){
+           $ext = strtolower(pathinfo($image["name"],PATHINFO_EXTENSION)); 
+           $imageName = uniqid(""). "." . $ext; 
+           $destination = "assets/{$imageName}"; 
+           if($source == "courses"){
+               $destination = "../assets/{$imageName}"; 
+           }
+           move_uploaded_file($image["tmp_name"], $destination); 
+       }
+
+       return [$imageName, $message]; 
+   }
+
+?>
+
