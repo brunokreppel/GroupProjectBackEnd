32dad6471d136cdc7ae8d09afac5fd18746a5f8b
Richard Waidacher <rw.tone@gmail.com>
2023-12-12 15:51:42 +0100

    footer added

---
+++ components/footer.php
@@ -0,0 +1,87 @@
+
+
+<?php
+echo "
+
+<footer bg-body-tertiary class='text-center'>
+  <!-- Grid container -->
+  <div class='container p-4 pb-0'>
+   
+  
+  <!-- Section: Social media -->
+    <section class='mb-4'>
+
+      <!-- Facebook -->
+      <a
+      data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #3b5998;'
+        href='#!'
+        role='button'
+        ><i class='ri-facebook-fill'></i></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #55acee;'
+        href='#!'
+        role='button'
+        ><i class='ri-twitter-x-fill'></i></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #dd4b39;'
+        href='#!'
+        role='button'
+        ><i class='ri-google-fill'></i></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #ac2bac;'
+        href='#!'
+        role='button'
+        ><i class='ri-instagram-line'></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #0082ca;'
+        href='#!'
+        role='button'
+        ><i class='ri-linkedin-fill'></i
+      ></a>
+      <!-- Github -->
+      <a
+        data-mdb-ripple-init
+        class='btn text-white btn-floating m-1'
+        style='background-color: #333333;'
+        href='#!'
+        role='button'
+        ><i class='ri-github-line'></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class='text-center p-3' style='background-color: rgba(0, 0, 0, 0.05);'>
+    © 2023 Copyright:
+    <a class='text-body' href='#'>Tudors Sphere</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+";
+
+?>
