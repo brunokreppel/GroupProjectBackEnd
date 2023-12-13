3599936cecac4013aa759868c0d312bee6f4c246
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-13 10:13:05 +0100

    adding images for cources

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ require_once "components/navbar.php";
     <link rel="stylesheet" href="style/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
 
 </head>
 <body>
@@ -51,7 +55,7 @@ require_once "components/navbar.php";
 
 
 <div class="tutor_wrapper">
-
+    
 </div>
 
 <div class="review_wrapper">
@@ -62,7 +66,9 @@ require_once "components/navbar.php";
 <?php require_once 'components/footer.php' ?>
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+     
 </body>
 </html>
