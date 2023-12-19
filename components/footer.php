32fdc6cf684d7dbec60674df1bb1957ba36946d3
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-19 10:11:53 +0100

    fixing style for footer

--- components/footer.php
+++ components/footer.php
@@ -2,96 +2,115 @@
 
 <?php
 echo "
+
+<style>
+
+ .footer{
+  font-family: 'Bai Jamjuree', sans-serif!important;
+  } 
+
+  *{
+    font-family: 'Bai Jamjuree', sans-serif !important;
+  }
+
+  .copyright{
+    text-decoration: none;
+  }
+
+  .copyright:hover{
+    color: #FFFFFF!important;
+  }
+
+</style>
+
 <div class='container my-5'>
 
-    <footer class='text-white text-center text-lg-start bg-dark'>
-        <!-- Grid container -->
-        <div class='container p-4'>
-            <!--Grid row-->
-            <div class='row mt-4'>
-                <!--Grid column-->
-                <div class='col-lg-4 col-md-12 mb-4 mb-md-0'>
-                    <h5 class='text-uppercase mb-4'>Contact</h5>
-
-                    <ul class='fa-ul' style='margin-left: 1.65em;'>
-                        <li class='mb-3'>
-                            <span class='fa-li'><i class='fas fa-home'></i></span><span class='ms-2'>Karmeliterpl. 5/17,
-                                1020 Vienna</span>
-                        </li>
-                        <li class='mb-3'>
-                            <span class='fa-li'><i class='fas fa-envelope'></i></span><span
-                                class='ms-2'>tutorsphere@mail.com</span>
-                        </li>
-                        <li class='mb-3'>
-                            <span class='fa-li'><i class='fas fa-phone'></i></span><span class='ms-2'>+ 43 xxx xxx xx
-                                xx</span>
-                        </li>
-                    </ul>
-
-                    <div class='mt-4'>
-                        <!-- Facebook -->
-                        <a type='button' class='btn btn-floating btn-light btn-lg'><i class='fab fa-facebook-f'></i></a>
-                        <!-- Dribbble -->
-                        <a type='button' class='btn btn-floating btn-light btn-lg'><i
-                                class='fab fa-linkedin-in'></i></a>
-                        <!-- Twitter -->
-                        <a type='button' class='btn btn-floating btn-light btn-lg'><i class='fab fa-twitter'></i></a>
-                        <!-- Google + -->
-                        <a type='button' class='btn btn-floating btn-light btn-lg'><i
-                                class='fab fa-google-plus-g'></i></i></a>
-                        <!-- Linkedin -->
-                    </div>
-                </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class='col-lg-3 col-md-6 mb-4 mb-lg-0'>
-                    <h5 class='text-uppercase mb-4'>Sign up to our newsletter</h5>
-
-                    <div class='form-outline form-white mb-4'>
-                        <input type='email' id='form5Example2' class='form-control' />
-                        <label class='form-label' for='form5Example2'>Email address</label>
-                    </div>
-
-                    <button type='submit' class='btn btn-light btn-block'>Subscribe</button>
-                </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class='col-lg-4 col-md-6 mb-4 mb-md-0'>
-                    <h5 class='text-uppercase mb-4'>Opening hours</h5>
-
-                    <table class='table text-center text-white'>
-                        <tbody class='fw-normal'>
-                            <tr>
-                                <td>Mon - Wed:</td>
-                                <td>8am - 8pm</td>
-                            </tr>
-                            <tr>
-                                <td>Tue - Thu:</td>
-                                <td>10am - 6pm</td>
-                            </tr>
-                            <tr>
-                                <td>Fri - Sat:</td>
-                                <td>10am - 2pm</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!--Grid column-->
-            </div>
-        </div>
+    <footer class='text-white text-center text-lg-start bg-dark myFooter'>
+    <!-- Grid container -->
+    <div class='container p-4'>
         <!--Grid row-->
-</div>
-<!-- Grid container -->
+        <div class='row mt-4'>
+            <!--Grid column-->
+            <div class='col-lg-4 col-md-12 mb-4 mb-md-0'>
+            <h5 class='text-uppercase mb-4'>Meet us</h5>
 
-<!-- Copyright -->
-<div class='text-center p-3' style='background-color: rgba(0, 0, 0, 0.2);'>
-    © 2023 Copyright:
-    <a class='text-white' href='#'>TutorSphere</a>
-</div>
-<!-- Copyright -->
-</footer>
+                <ul class='fa-ul' style='margin-left: 1.65em;'>
+                    <li class='mb-3'>
+                        <span class='fa-li'><i class='ri-home-8-line'></i></span><span class='ms-2'>Karmeliterpl. 5/17,
+                            1020 Vienna</span>
+                    </li>
+                    <li class='mb-3'>
+                        <span class='fa-li'><i class='ri-mail-line'></i></span><span
+                            class='ms-2'>tutorsphere@mail.com</span>
+                    </li>
+                    <li class='mb-3'>
+                        <span class='fa-li'><i class='ri-phone-line'></i></span><span class='ms-2'>+ 43 xxx xxx xx
+                            xx</span>
+                    </li>
+                </ul>
+
+                <div class='mt-4'>
+                    <!-- Facebook -->
+                    <a type='button' class='btn btn-floating btn-light btn-lg'><i class='ri-facebook-fill'></i></a>
+                    <!-- Linkedin -->
+                    <a type='button' class='btn btn-floating btn-light btn-lg'><i class='ri-linkedin-fill'></i
+                    ></i></a>
+                    <!-- Twitter -->
+                    <a type='button' class='btn btn-floating btn-light btn-lg'><i class='ri-twitter-x-line'></i></a>
+                    <!-- Google + -->
+                    <a type='button' class='btn btn-floating btn-light btn-lg'><i class='ri-google-fill'></i></a>
+                </div>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class='col-lg-3 col-md-6 mb-4 mb-lg-0'>
+                <h5 class='text-uppercase mb-4'>Sign up to our newsletter</h5>
+
+                <div class='form-outline form-white mb-4'>
+                    <input type='email' id='form5Example2' class='form-control' />
+                    <label class='form-label' for='form5Example2'>Email address</label>
+                </div>
+
+                <button type='submit' class='btn btn-light btn-block'>Subscribe</button>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class='col-lg-4 col-md-6 mb-4 mb-md-0'>
+                <h5 class='text-uppercase mb-4'>Opening hours</h5>
+
+                <table class='table text-center text-white'>
+                    <tbody class='fw-normal'>
+                        <tr>
+                            <td>Mon - Wed:</td>
+                            <td>8am - 8pm</td>
+                        </tr>
+                        <tr>
+                            <td>Tue - Thu:</td>
+                            <td>10am - 6pm</td>
+                        </tr>
+                        <tr>
+                            <td>Fri - Sat:</td>
+                            <td>10am - 2pm</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!--Grid column-->
+        </div>
+    </div>
+    <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class='text-center p-3' style='background-color: rgba(0, 0, 0, 0.2);'>
+      © 2023 Copyright:
+      <a class='text-white text-reset copyright' href='#'>TutorSphere</a>
+    </div>
+    <!-- Copyright -->
+    </footer>
 
 </div>
 ";
