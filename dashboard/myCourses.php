3790a7a2193bc85d748808b1cf68b21859d33cf5
bruno kreppel <bruno.kreppel@gmail.com>
2023-12-19 14:27:58 +0100

    changed my courses

--- dashboard/myCourses.php
+++ dashboard/myCourses.php
@@ -1,10 +1,11 @@
 <?php
+// Start the session and include necessary files
 session_start();
 require_once '../components/db_Connect.php';
 $loc = "../";
 require_once "../components/navbar.php";
 
-// checks if the session is TUTOR
+// Check if the user is a TUTOR; otherwise, redirect to the index page
 if (!isset($_SESSION["TUTOR"])) {
     header("Location: ../index.php");
     die();
@@ -17,6 +18,7 @@ if (!isset($_GET['id']) || empty($_GET['id'])) {
     die();
 }
 
+// Get user IDs from session and URL
 $sessionUserId = $_SESSION['TUTOR'];
 $urlUserId = $_GET['id'];
 
@@ -27,7 +29,7 @@ if ($sessionUserId !== $urlUserId) {
     exit();
 }
 
-// Modify the SQL query to filter by tutor ID
+// Build SQL query to fetch course information for the specific tutor
 $sql = "SELECT 
             course.id AS course_id, 
             course.fk_subject_id, 
@@ -68,122 +70,152 @@ $sql = "SELECT
             users ON course.fk_tutor_id = users.id
         WHERE 
             course.fk_tutor_id = {$_GET['id']}"; 
+
+// Execute the query
 $result = mysqli_query($conn, $sql);
 $cards = "";
 
+// Check if the query was successful
 if ($result) {
+    // Loop through the fetched results and build HTML cards
     while ($row = mysqli_fetch_assoc($result)) {
         $cards .= "
-           <div class='col mb-4'>
-                <div class='card h-100 shadow-sm'>
-                    <img src='../assets/{$row['course_image']}' class='bd-placeholder-img card-img-top' width='100%' height='225' role='img' alt='Course Image'>
-                    <div class='card-body'>
-                        <h5 class='card-title fw-bold'>{$row['subject_name']}</h5>
-                        <p class='card-text'>{$row['university_name']}</p>
-                        <div class='d-flex justify-content-between align-items-center mt-auto'>
-                            <div class='btn-group'>
-                                <a href='studentList.php?id={$row['course_id']}' class='btn btn-sm btn-outline-secondary'>Details <i class='ri-information-line'></i></a>
-                                <a href='../courses/updateCourse.php?id={$row['course_id']}' class='btn btn-sm btn-outline-secondary'>Update <i class='ri-pencil-line'></i></a>
-                            </div>
-                            <small class='text-body-secondary'>From: " . date('F j, Y', strtotime($row['fromDate'])) . "</small>
-                            <small class='text-body-secondary'>To: " . date('F j, Y', strtotime($row['ToDate'])) . "</small>
-                        </div>
-                    </div>
+        <article class='postcard light blue test'>
+            <!-- Course Image -->
+            <a class='postcard__img_link' href='#'>
+                <img class='postcard__img' src='../assets/{$row['course_image']}' alt='Course Image' />
+            </a>
+            <div class='postcard__text t-dark'>
+                <!-- Course Title -->
+                <h1 class='postcard__title blue'><a href='studentList.php?id={$row['course_id']}'>{$row['subject_name']}</a></h1>
+                <div class='postcard__subtitle small'>
+                    <!-- Course Dates -->
+                    <time datetime='{$row['fromDate']}'>
+                        <i class='fas fa-calendar-alt mr-2'></i>From: " . date('F j, Y', strtotime($row['fromDate'])) . "
+                    </time>
+                    <time datetime='{$row['ToDate']}'>
+                        <i class='fas fa-calendar-alt mr-2'></i>To: " . date('F j, Y', strtotime($row['ToDate'])) . "
+                    </time>
                 </div>
-            </div>";
+                <div class='postcard__bar'></div>
+                <!-- University Name -->
+                <div class='postcard__preview-txt'>{$row['university_name']}</div>
+                <!-- Actions: Details, Students, Update -->
+                <ul class='postcard__tagbox'>
+                    <li class='tag__item play blue'>
+                        <a href='../courses/courseDetails.php?id={$row['course_id']}'><i class='fas fa-play mr-2'></i>Details</a>
+                    </li>
+                    <li class='tag__item play blue'>
+                        <a href='studentList.php?id={$row['course_id']}'><i class='fas fa-play mr-2'></i>Students</a>
+                    </li>
+                    <li class='tag__item'>
+                        <a href='../courses/updateCourse.php?id={$row['course_id']}'><i class='fas fa-edit mr-2'></i>Update</a>
+                    </li>
+                </ul>
+            </div>
+        </article>";
     }
 } else {
+    // Display an error message if the query fails
     echo "Error: " . mysqli_error($conn);
 }
 
+// Close the database connection
 mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Courses</title>
+    <!-- Fonts and stylesheets -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
+    <link rel="stylesheet" href="../style/card.css">
     <style>
-        /* Add your custom styles here */
+        /* Custom styles */
+        *{font-family: 'Bai Jamjuree', sans-serif;}
         #noResultsMessage {
             display: none;
         }
         .headerH1{
-    text-align: center;
-    font-size: 2.6rem !important;
-    font-weight: 700;
-    margin: 30px 0 5px 0 !important;
-    
-}
-.iconH1{
-    font-size: 2.6rem;    
-
-}
+            text-align: center;
+            font-size: 2.6rem !important;
+            font-weight: 700;
+            margin: 0 0 5px 0 !important;
+            color: black;
+        }
+        .iconH1{
+            font-size: 2.6rem;    
+        }
     </style>
 </head>
 <body>
-<section class=" text-center container">
-    <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="headerH1">Your Courses <i class="ri-book-read-line iconH1"></i></h1>
-
-            <p class="lead text-body-secondary">View and Edit your own Courses.</p>
-
-            <div class="container ">
-    <input type="text" id="searchInput" class="form-control" placeholder="Search by course name...">
-</div>
+    <section class="text-center container">
+        <div class="row">
+            <div class="col-lg-6 col-md-8 mx-auto mt-4">
+                <!-- Header -->
+                <h1 class="headerH1">Your Courses <i class="ri-book-read-line iconH1"></i></h1>
+                <p class="lead text-body-secondary">View and Edit your own Courses.</p>
+                <!-- Search input -->
+                <div class="container ">
+                    <input type="text" id="searchInput" class="form-control" placeholder="Search by course name...">
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-
-
-<div class="album py-5">
-    <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 d-flex justify-content-center">
+    <section class="light">
+        <div class="container py-5">
+            <!-- Display course cards -->
             <?php echo $cards ?>
+            <!-- No results message -->
             <p id="noResultsMessage" class="text-center fw-bold">Nothing Found...</p>
         </div>
-    </div>
-</div>
+    </section>
 
-<?php require_once '../components/footer.php' ?>
+    <!-- Footer -->
+    <?php require_once '../components/footer.php' ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const searchInput = document.getElementById("searchInput");
-        const cards = document.querySelectorAll(".col");
-        const noResultsMessage = document.getElementById("noResultsMessage");
+    <!-- Custom script for search functionality -->
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const searchInput = document.getElementById("searchInput");
+            const cards = document.querySelectorAll(".test");
+            const noResultsMessage = document.getElementById("noResultsMessage");
 
-        // Set the initial state to 'none'
-        noResultsMessage.style.display = 'none';
+            // Set the initial state to 'none'
+            noResultsMessage.style.display = 'none';
 
-        function performSearch() {
-            const query = searchInput.value.trim().toLowerCase();
-            let foundResults = false;
+            function performSearch() {
+                const query = searchInput.value.trim().toLowerCase();
+                let foundResults = false;
 
-            cards.forEach(card => {
-                const cardText = card.textContent.toLowerCase();
-                const match = cardText.includes(query);
-                card.style.display = match ? 'flex' : 'none';
+                cards.forEach(card => {
+                    const cardText = card.textContent.toLowerCase();
+                    const match = cardText.includes(query);
+                    card.style.display = match ? 'flex' : 'none';
 
-                if (match) {
-                    foundResults = true;
-                }
-            });
+                    if (match) {
+                        foundResults = true;
+                    }
+                });
 
-            // Toggle the display of the noResultsMessage based on search results
-            noResultsMessage.style.display = foundResults ? 'none' : 'block';
-        }
+                // Toggle the display of the noResultsMessage based on search results
+                noResultsMessage.style.display = foundResults ? 'none' : 'block';
+            }
 
-        searchInput.addEventListener("input", performSearch);
-    });
-</script>
+            searchInput.addEventListener("input", performSearch);
+        });
+    </script>
 </body>
 </html>
