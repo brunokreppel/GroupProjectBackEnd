0539ade9f35d98105784938a61397ccddeddecfd
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-20 09:21:50 +0100

    adding link font awesome

--- courses/courseDetails.php
+++ courses/courseDetails.php
@@ -241,6 +241,7 @@ if ($result) {
     <link rel="stylesheet" href="style/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
+    <script src="https://kit.fontawesome.com/a32278c845.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
--- courses/courses.php
+++ courses/courses.php
@@ -109,6 +109,7 @@ mysqli_close($conn);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
     <link rel="stylesheet" href="../style/card.css">
+    <script src="https://kit.fontawesome.com/a32278c845.js" crossorigin="anonymous"></script>
 
     <style>
         /* Custom styles */
