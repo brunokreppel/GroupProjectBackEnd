7f2032266ce711bf42bbbaa42b3ddd58ed9dd769
bruno kreppel <bruno.kreppel@gmail.com>
2023-12-21 10:38:10 +0100

    changed reviews

--- reviews/reviews.php
+++ reviews/reviews.php
@@ -44,7 +44,7 @@ if ($result) {
                 " . $ratingStars ."
                 </div>
                 <h1 class='text-body-emphasis CstmH1'>{$row['subject_name']}</h1>
-                <p class='col-lg-8 mx-auto mb-4 fst-italic reviewP'>
+                <p class='col-lg-10 mx-auto mb-4 fst-italic reviewP overflow-scroll px-2'>
                     {$row['review_message']}
                 </p>
                 <div class='d-flex justify-content-center p-2'>
@@ -102,7 +102,8 @@ mysqli_close($conn);
 
 <div class="container mb-5 mt-4" style='min-height: 83dvh;'>
         <h1 class="headerH1 pb-5">Reviews <i class="ri-double-quotes-l"></i></h1>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-xl-2 g-3">
+
             
                 <?= $cards ?>
             
