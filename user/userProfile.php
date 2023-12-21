9583bb1170e6d8c35ab5606fbdf868ffc8e28324
bruno kreppel <bruno.kreppel@gmail.com>
2023-12-21 09:02:37 +0100

    removed style

--- user/userProfile.php
+++ user/userProfile.php
@@ -206,7 +206,6 @@ mysqli_close($conn);
     <link rel="stylesheet" href="../style/rootstyles.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/php/BE20_CR5_BrunoKreppel/style/stylesheet.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
