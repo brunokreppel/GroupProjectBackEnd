3f0faa3f5a07069d045f98e1afa3feeff84a6ad3
BarnoMamadzhanova <b@mamadzhanova.com>
2024-01-18 22:36:27 +0100

    fixing db connection for deploying

--- components/db_connect.php
+++ components/db_connect.php
@@ -1,8 +1,8 @@
 <?php 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$dbName = "be20_grp_tutoring";
+$user = "u773418918_tutor_manager";
+$pass = "u>x?H#jW1";
+$dbName = "u773418918_tutor_db";
 
 $conn = mysqli_connect($host, $user, $pass, $dbName);
 ?>
